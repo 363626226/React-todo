992a074a329ccc8335e0bb5da632af1c530ccef3
castor <p30549095p@gmail.com>
2022-10-27 18:33:21 +0800

    complete

---
+++ src/pages/Home/components/Item.js
@@ -0,0 +1,25 @@
+const Item = ({id,note,date,time,deleteData,submittingStatus}) => {
+
+    function deleteItem(){
+        submittingStatus.current = true
+        deleteData(function(prev) { 
+            return prev.filter(item => item.id !== id)
+        })
+    }
+
+    return <div className="item">
+        <div>
+            <p>{note}</p>
+            <p>{`${date} ${time}`}</p>
+        </div>
+        <button onClick={deleteItem} className="remove">刪除</button>
+    </div>
+}
+
+// {
+//     "id":"1",
+//     "note":"11",
+//     "date":"",
+//     "time":""
+//   }
+export default Item
